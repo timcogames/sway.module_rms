4e11e509f1b7a643f83999639d2b8bcf38778d47
apriori85 <victor-timoshin@hotmail.com>
2023-01-10 10:26:38 +0300

    Added sway.module_loader submodule

--- lib/src/resourceprovider.cpp
+++ lib/src/resourceprovider.cpp
@@ -1,3 +1,4 @@
+#include <sway/loader.hpp>
 #include <sway/rms/resourceprovider.hpp>
 
 #include <iostream>
@@ -5,12 +6,16 @@
 NAMESPACE_BEGIN(sway)
 NAMESPACE_BEGIN(rms)
 
+using PluginGetDescriptorFunc_t = core::binding::TFunction<loader::LoaderPluginDescriptor<loader::ImageLoaderPlugin>()>;
+
 ResourceProvider::ResourceProvider() {
   try {
     plug_ = new core::Plugin(core::generic::io::Path(
         "/Users/apriori85/Documents/Projects/sway.module_rms/bin/module_loader_png.dylib.0.1.0"));
     core::PluginInfo const info = plug_->getInfo();
-    std::cout << "Plugin name: " << info.name << std::endl;
+    auto instance = plug_->getMethod<PluginGetDescriptorFunc_t>("pluginGetLoader");
+
+    std::cout << "Plugin: " << info.name << " " << instance().plug()->getText() << std::endl;
   } catch (const std::exception &err) {
     std::cout << err.what() << std::endl;
   }
